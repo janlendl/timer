13ec6856e9de9d7c32cb8bed1953db8beec47831
Lendl Peralta <janlendl@gmail.com>
2021-08-14 23:47:56 +0000

    finished timer

--- timer1.js
+++ timer1.js
@@ -1,3 +1,9 @@
+// use process.argv to get user input. ie: timer1.js 5 10 15 20 where the numbers will be how many secs for every beep
 const alarmTimer = process.argv.slice(2);
-console.log(alarmTimer)
-// setTimeout(() => console.log('\x07'), 1250)
+const sortedTimer = alarmTimer.sort((a,b) => a - b);
+
+for (const i of sortedTimer) {
+  if (i > 0) {
+    setTimeout(() => process.stdout.write('\x07'), i * 1000);
+  }
+}
