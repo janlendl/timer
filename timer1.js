2e827288448bf22b9dd8022f4735ffd765ae87c8
Lendl Peralta <janlendl@gmail.com>
2021-08-21 23:53:28 +0000

    refactor code with @SomeChineseGuy during my code review with him.

--- timer1.js
+++ timer1.js
@@ -1,9 +1,9 @@
 // use process.argv to get user input. ie: timer1.js 5 10 15 20 where the numbers will be how many secs for every beep
 const alarmTimer = process.argv.slice(2);
-const sortedTimer = alarmTimer.sort((a,b) => a - b);
-
-for (const i of sortedTimer) {
-  if (i > 0) {
+alarmTimer
+  .sort((a,b) => a - b)
+  .forEach(i => {
+  if (typeof Number(i) === 'number' && i > 0) {
     setTimeout(() => process.stdout.write('\x07'), i * 1000);
   }
-}
+});
